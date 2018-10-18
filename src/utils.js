7fbde7b61cd74d90281265c19a3b80773e9dc46c
Romain Gallay <romain.gallay@heig-vd.ch>
2018-10-18 13:44:36 +0200

    remove duplicate elements from response

--- src/utils.js
+++ src/utils.js
@@ -13,17 +13,17 @@ function getReposLanguagesStats(reposLanguages = []) {
 
 function getData(items = []) {
   const tab = ['README', 'LICENSE', 'CONTRIBUTING', 'test', 'eslint'];
-  const response = [];
+  const response = new Set();
   tab.forEach(word => {
     items.forEach(item => {
       if (item.name.includes(word)) {
         // if there is a word in the item,
         // i.e return true if the word is README and the item.name is README.md
-        response.push({ name: word, value: true });
+        response.add(word);
       }
     });
   });
-  return response;
+  return Array.from(response);
 }
 
 
